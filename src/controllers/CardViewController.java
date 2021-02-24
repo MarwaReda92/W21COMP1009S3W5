cea222bcdcb35d8dced1e1f6928293f4433f4dc4
Mara <marwareda1992@hotmail.com>
2021-02-24 18:19:44 -0500

    Updated so that the variables of each card would be displayed within the labels of the view.

--- src/controllers/CardViewController.java
+++ src/controllers/CardViewController.java
@@ -1,4 +1,37 @@
 package controllers;
 
-public class CardViewController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import models.*; //anything in this package should be included here, let them talk
+import models.DeckOfCards;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class CardViewController implements Initializable { //initializable being implemented means not only are we
+    // creating a cardview controller class, we are also creating an instance of the initializable interface which
+    // means anythign that an initializable object can do, we can do
+
+    @FXML //is telling the java class that it can interact with the  item in the view
+    private Label faceNameLabel;
+
+    @FXML
+    private Label suitLabel;
+
+    @FXML
+    private Label faceValueLabel;
+
+    private DeckOfCards deckOfCards; //inside of the class but outside of the method
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) { //this is like in the main method where we
+        //extended the application class and we were forced to have the method Start
+        deckOfCards = new DeckOfCards();
+        Card card = deckOfCards.dealTopCard(); //this will take the first card at the  top of the deck and place it into card
+        faceNameLabel.setText(card.getFaceName()); //the card object will give us back the faceName and update the label
+        suitLabel.setText(card.getSuit());
+        faceValueLabel.setText(Integer.toString(card.getFaceValue())); //using the  int wrapper class to convert int to String
+    }
 }
---
+++ src/models/Card.java
@@ -0,0 +1,92 @@
+package models;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Card {
+    //Instance variables - the attributes we will model in the class
+    private String faceName, suit; //face name is Ace/jack etc
+    private int faceValue;
+
+    /**
+     * The goal of the constructor is to allocate memory for the object. When this is called it will
+     * reserve space and return a "pointer" to the object.
+     *
+     * A constructor does not have a "return" type and it must match the name of the class
+     */
+
+    public Card(String face, String suit, int faceValue) //constructor with 3 arguments
+    {
+        setFaceName(face);
+        setSuit(suit);
+        setFaceValue(faceValue);
+    }
+
+    public String getFaceName() {
+        return faceName;
+    }
+
+    /**
+    * This returns a list of all the valid face names - static so it can be used in other classes
+    */
+    public static List<String> getFaceNames(){ //static so that it is common to the Card class, do not need an instance of the class to call this method
+        return Arrays.asList("two","three","four","five","six","seven","eight",
+                "nine","ten","jack","queen","king","ace");
+    }
+
+    /**
+     * This will ensure that the face name represents a valid face name.
+     * @param faceName - two, three, four,...,ten, jack, queen, king, ace
+     */
+    public void setFaceName(String faceName) {
+        faceName = faceName.toLowerCase();
+
+        List<String> validFaceNames = getFaceNames();
+        if (validFaceNames.contains(faceName))
+            this.faceName = faceName;
+        else
+            throw new IllegalArgumentException(faceName +" is not valid, choose from: "+ validFaceNames);
+    }
+
+    public String getSuit() {
+        return suit;
+    }
+
+    /**
+     * This method will validate the the argument is hearts, diamonds, spades or clubs
+     * and set the instance variable
+     * @param suit
+     */
+    public void setSuit(String suit) {
+        suit = suit.toLowerCase();
+        List<String> validSuits = getSuits();
+        if (validSuits.contains(suit))
+            this.suit = suit;
+        else
+            throw new IllegalArgumentException(suit + " is not valid, valid options are: "+validSuits);
+    }
+
+    public static List<String> getSuits(){
+        return Arrays.asList("hearts","diamonds","spades","clubs");
+    }
+
+    public int getFaceValue() {
+        return faceValue;
+    }
+
+    /**
+     *This represents the card value is between 2-14 and sets the instance variable
+     * @param faceValue (values 2-14)
+     */
+    public void setFaceValue(int faceValue) {
+        if (faceValue>=2 && faceValue<=14)
+            this.faceValue = faceValue;
+        else
+            throw new IllegalArgumentException("face value must be 2-14 inclusive");
+    }
+
+    public String toString()
+    {
+        return faceName + " of " + suit;
+    }
+}
